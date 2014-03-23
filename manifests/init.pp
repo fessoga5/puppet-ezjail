67cc9b1f61f33a95f9ee1a7006e3d9fc87d6c7e3
root <root@d1stkfactory>
2014-03-23 08:05:30 +0000

    add ezjail-install

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,27 @@
 #
 #INSTALL SKYPE ON DESKTOP
 #
-class puppet-ezjail {
+class puppet-ezjail ($force_install = false) {
+
+    #variables
+    $path_freebsd = ["/bin", "/sbin","/usr/bin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin"]
+    #
     #Class for puppet
     package {"ezjail":
         ensure => installed,
         provider => pkgng,
     }
+
+    #exec install ezjail
+    $unless_test = $force_install ? {
+        true  => '/bin/test',
+        false => "/bin/test -d /usr/jails/basejail/bin",
+    }
+
+    exec {"ezjail-admin install":
+        unless => $unless_test,
+        path    => $path_freebsd,
+    }
+
+
 }
