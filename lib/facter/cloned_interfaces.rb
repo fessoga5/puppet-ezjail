0c1fc6e668bbf133b5e5219edfbd032e31cb71e4
fess <fess@laptop>
2014-03-26 00:06:28 +0900

    add fact cloned_interfaces for adding on rc.conf

---
+++ lib/facter/cloned_interfaces.rb
@@ -0,0 +1,7 @@
+#Cloned interfaces
+
+Facter.add("cloned_interfaces") do
+  setcode do
+    Facter::Util::Resolution.exec('/usr/local/bin/augtool -i get /files/etc/rc.conf/cloned_interfaces | /usr/bin/awk -F "=" '{print $2}' | /usr/bin/sed 's/\"//g'')
+  end
+end
