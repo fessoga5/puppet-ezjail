80cccda48f25648a4045f754c2de3294836b77dd
fess <fess@laptop>
2014-03-26 00:17:23 +0900

    fix rb

--- lib/facter/cloned_interfaces.rb
+++ lib/facter/cloned_interfaces.rb
@@ -2,6 +2,6 @@
 
 Facter.add("cloned_interfaces") do
   setcode do
-    Facter::Util::Resolution.exec('/usr/local/bin/augtool -i get /files/etc/rc.conf/cloned_interfaces')
+    Facter::Util::Resolution.exec('/usr/local/bin/augtool -i get /files/etc/rc.conf/cloned_interfaces | /usr/bin/awk \-F\"=\" \{ \' print $2 \' \} ')
   end
 end
