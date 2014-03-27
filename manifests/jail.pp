245a1585ad960f02af35b3e61f748f0d665a1cb9
fess <fess@laptop>
2014-03-28 00:49:06 +0900

    fix other bugs

--- manifests/jail.pp
+++ manifests/jail.pp
@@ -92,7 +92,7 @@ define puppet-ezjail::jail (
 		exec { "create-ezjail":
 			command => "ezjail-admin create -r $jail_rootdir/$jail_hostname $jail_name $jail_ipaddress",
 			path => $path_freebsd,
-			unless => '/bin/test -d $jail_rootdir/$jail_hostname'
+			unless => '/bin/test -s $jail_rootdir/$jail_hostname'
 		}
 	}
 	
