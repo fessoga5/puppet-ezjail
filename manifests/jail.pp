4847cf63c3463f46402112cc6a765f567d2ac562
fess <fess@laptop>
2014-03-28 01:31:05 +0900

    fix other bugs

--- manifests/jail.pp
+++ manifests/jail.pp
@@ -26,51 +26,52 @@ define puppet-ezjail::jail (
     #Create interfaces in rc.conf
     if ($autostart == true) {
         #Add interface to rc.conf
-        augeas {"rc.conf_epairs":
+        augeas {"rc.conf_epairs_${name}":
             context => "/files/etc/rc.conf",
             changes => ["set cloned_interfaces '\"$cloned_interfaces $vnet_interface\"'"],
             onlyif => "match cloned_interfaces[. =~ regexp('.*$vnet_interface.*')] size == 0",
         }
         
         #Add interface ipaddress to rc.conf
-        augeas {"rc.conf_inet_epairs":
+        augeas {"rc.conf_inet_epairs_${name}":
             context => "/files/etc/rc.conf",
             changes => ["set ifconfig_${vnet_interface}a '\"inet ${inet_epair_a} netmask ${netmask_epair_a}\"'"],
             onlyif => "match ifconfig_${vnet_interface}a[. =~ regexp('.*inet ${inet_epair_a} netmask ${netmask_epair_a}.*')] size == 0",
-            require => Augeas["rc.conf_epairs"],
+            require => Augeas["rc.conf_epairs_${name}"],
         }
 
     }
 
     if ($create_ifaces_epair == true) {
         #create epair
-		exec { "create_epair":
+		exec { "create_epair_${name}":
 			command => "ifconfig $vnet_interface create ",
 			path => $path_freebsd,
 			unless => "/sbin/ifconfig ${vnet_interface}a"
 		}
 		
         #inet for epair
-        exec { "inet_epair":
+        exec { "inet_epair_${name}":
 			command => "ifconfig ${vnet_interface}a ${inet_epair_a} netmask ${netmask_epair_a} up",
 			path => $path_freebsd,
-            require => Exec["create_epair"],
+            require => Exec["create_epair_${name}"],
 			unless => "/sbin/ifconfig ${vnet_interface}a | /usr/bin/grep ${inet_epair_a}"
 		}
 
     }
     #
 	$require_test = $create ? {
-		true  => Exec["create-ezjail"],
+		true  => Exec["create-ezjail_${name}"],
 		false => undef, 
 	}
 
 	$restart_jail = $restart_on_change ? {
-		true => File["$conf_dir/$jail_name"],
+		true => File["config_${name}"],
 		false => undef, 
 	}
 
-	file { "$conf_dir/$jail_name":
+	file { "config_${name}":
+        path => "$conf_dir/$jail_name",
 		replace => "yes",
 		owner   => $owner,
 		mode    => 600,
@@ -78,7 +79,7 @@ define puppet-ezjail::jail (
 		require => $require_test,
 	}
 
-	exec{"restart_jail": 
+	exec{"restart_jail_${name}": 
 		command => "ezjail-admin restart $jail_hostname", 
 		path => $path_freebsd,
 		refreshonly  => true,
@@ -89,7 +90,7 @@ define puppet-ezjail::jail (
 	#Template for new jail
 
 	if ( $create == true ) {
-		exec { "create-ezjail":
+		exec { "create-ezjail_${name}":
 			command => "ezjail-admin create -r $jail_rootdir/$jail_hostname $jail_name $jail_ipaddress",
 			path => $path_freebsd,
 			unless => '/bin/test -b $jail_rootdir/$jail_hostname'
@@ -97,10 +98,10 @@ define puppet-ezjail::jail (
 	}
 	
 	if ( $running == true ){
-		exec { "running-ezjail":
+		exec { "running-ezjail_${name}":
 			command => "ezjail-admin start $jail_hostname",
 			path => $path_freebsd,
-			require => File["$conf_dir/$jail_name"],
+			require => File["config_${name}"],
 			unless => "/usr/sbin/jls | /usr/bin/grep $jail_hostname"
 		}
 	}	
