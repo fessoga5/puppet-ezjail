b91d43febe443488f63a17ec4171b822b8f74144
fess <fess@laptop>
2014-03-28 00:26:28 +0900

    fix other bugs

--- manifests/jail.pp
+++ manifests/jail.pp
@@ -36,7 +36,7 @@ define puppet-ezjail::jail (
         augeas {"rc.conf_inet_epairs":
             context => "/files/etc/rc.conf",
             changes => ["set ifconfig_${vnet_interface}a '\"inet ${inet_epair_a} netmask ${netmask_epair_a}\"'"],
-            onlyif => "match ifconfig_${vnet_interface}a[. =~ regexp('.*inet ${inet_epair_a}.*')] size == 0",
+            onlyif => "match ifconfig_${vnet_interface}a[. =~ regexp('.*inet ${inet_epair_a} netmask ${netmask_epair_a}.*')] size == 0",
             require => Augeas["rc.conf_epairs"],
         }
 
